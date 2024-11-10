cf6b5a17d7c69b438f58d3cea113b4963fa732b6
Vagoasdf <bruno.gutierrez96@gmail.com>
2024-11-10 20:42:07 -0300

    Vistas base y navegacion modo Django

--- TALLER_MARKETPLACE_INFORMATICO_ACEVEDO_ANDERS_ESPARZA/base_app/views.py
+++ TALLER_MARKETPLACE_INFORMATICO_ACEVEDO_ANDERS_ESPARZA/base_app/views.py
@@ -3,7 +3,7 @@ from base_app.models import Producto
 
 def index(request):
     productos_recientes = Producto.objects.all()[:8]  # Ejemplo: los 8 productos más recientes
-    categorias_populares = Producto.objects.values('categoria').distinct()[:4]  # Ejemplo: 4 categorías populares
+    categorias_populares = Producto.objects.values('id_cate').distinct()[:4]  # Ejemplo: 4 categorías populares
     productos_descuento = Producto.objects.filter(descuento__isnull=False)[:8]  # Ejemplo: productos con descuento
 
     context = {
@@ -13,4 +13,29 @@ def index(request):
     }
     return render(request, 'index.html', context)
 
+def productos(request):
+    productos = Producto.objects.all()
+    context = {
+        'productos': productos,
+    }
+    return render(request, 'productos.html', context)
 
+def detalle_producto(request, id_producto):
+    producto = Producto.objects.get(id=id_producto)
+    context = {
+        'producto': producto,
+    }
+    return render(request, 'detalle_producto.html', context)
+
+def publicar(request):
+    ### TODO: Cambiar nombre?
+    return render(request, 'publicar.html')
+
+def perfil_cliente(request):
+    return render(request, 'perfil/perfil_cliente.html')
+
+def perfil_usuario(request):
+    return render(request, 'perfil/perfil_usuario.html')
+
+def editar_perfil_view(request):
+    return render (request, 'perfil/editar_perfil.html')
--- TALLER_MARKETPLACE_INFORMATICO_ACEVEDO_ANDERS_ESPARZA/marketplace/urls.py
+++ TALLER_MARKETPLACE_INFORMATICO_ACEVEDO_ANDERS_ESPARZA/marketplace/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from django.urls import path
-from base_app import views
+from django.urls import path,include
 
 urlpatterns = [
+    path('', include('base_app.urls')), 
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'),  # Ruta para la página principal
+
 ]
